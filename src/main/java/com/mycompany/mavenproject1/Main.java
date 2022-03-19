19455e495ac4a08965c9cbf4b61941baf1edc2b1
brianbacca <brianbn05@gmail.com>
2022-03-19 01:21:52 -0300

    first commit

---
+++ src/main/java/com/mycompany/mavenproject1/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+import com.mycompany.mavenproject1.forms.Form;
+
+/**
+ *
+ * @author 54348
+ */
+public class Main {
+    public static void main(String[]args){
+        Form ventana = new Form();
+        ventana.show();
+    }
+}
